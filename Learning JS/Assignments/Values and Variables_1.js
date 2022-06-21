9c77f4825afc56225e6caf65ebf4f176faaad33c
Kushal <sharankushal0@gmail.com>
2022-06-21 21:11:28 +0530

    1

---
+++ Learning JS/Assignments/Values and Variables_1.js
@@ -0,0 +1,5 @@
+let country = "India";
+let continent = "Asia";
+let population = "1394.3 million";
+console.log(`Country: ${country}\nContinent: ${continent}\nPopulation: ${population}\n`);
+console.log("Country: " + country + "\nContinent: " + continent + "\nPopulation: " + population);
